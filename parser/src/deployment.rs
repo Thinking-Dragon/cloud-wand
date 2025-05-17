d69b01345bb0b2fe908dc65526c004c2503b9a26
Thinking-Dragon <clement.gassmann-prince@canonical.com>
2025-05-17 17:41:36 -0400

    Look for definition files in all subdirectories of deployment directory as well as root

--- parser/src/deployment.rs
+++ parser/src/deployment.rs
@@ -1,6 +1,7 @@
-use std::{fs, path::PathBuf};
+use std::path::PathBuf;
 
 use anyhow::{bail, Result};
+use walkdir::WalkDir;
 
 use crate::{any_definition::AnyDefinition, deployment_definition::DeploymentDefinition, package_definition::PackageDefinition};
 
@@ -20,26 +21,27 @@ impl Deployment {
 
     pub fn from_directory(directory: PathBuf) -> Result<Self> {
         if !directory.is_dir() {
-            println!("{:#?}", directory);
             bail!("Deployment path must be a directory")
         }
 
         let mut deployment_definitions: Vec<DeploymentDefinition> = Vec::new();
         let mut package_definitions: Vec<PackageDefinition> = Vec::new();
 
-        for entry in fs::read_dir(directory)? {
-            let entry: PathBuf = entry?.path();
-            if entry.is_file() {
-                if let Some(extension) = entry.extension() {
-                    if extension == "yaml" {
-                        match AnyDefinition::parse(entry)? {
-                            AnyDefinition::Deployment(definition) => {
-                                deployment_definitions.push(definition);
-                            },
-                            AnyDefinition::Package(definition) => {
-                                package_definitions.push(definition);
-                            },
-                        }
+        for entry in WalkDir::new(directory)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|e| e.file_type().is_file())
+        {
+            let entry: PathBuf = PathBuf::from(entry.path());
+            if let Some(extension) = entry.extension() {
+                if extension == "yaml" {
+                    match AnyDefinition::parse(entry)? {
+                        AnyDefinition::Deployment(definition) => {
+                            deployment_definitions.push(definition);
+                        },
+                        AnyDefinition::Package(definition) => {
+                            package_definitions.push(definition);
+                        },
                     }
                 }
             }
